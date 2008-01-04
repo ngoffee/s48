cc6cb21949d31c63e583b03e0604e78d315f974b
Mike Sperber <sperber@deinprogramm.de>
2008-01-04 17:18:56 +0100

    Fix minor initial.image-<bits> build issues
    - don't open SCHEME in PLATFORM structure; instead, use
      DEFINE-ALL-OPERATORS
    - add dependencies for scheme/rts-packages-<bits>.scm

--- scheme/rts-packages-64.scm
+++ scheme/rts-packages-64.scm
@@ -4,5 +4,7 @@
 ; Platform-specific constants
 
 (define-structure platform platform-interface
-  (open scheme)
+  ;; don't open anything---this is loaded before even SCHEME-LEVEL-0 is available
+  (define-all-operators)
+  (usual-transforms and cond do let let* or)
   (files ((vm data) platform-64)))
