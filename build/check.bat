e81f0a3e9989a3fb5124075998950de26a61312d
Mike Sperber <sperber@deinprogramm.de>
2008-01-10 16:23:24 +0100

    Don't run UDP tests off "make check".
    They're too (inherently) unreliable.

--- build/check.bat
+++ build/check.bat
@@ -19,7 +19,7 @@ echo ,open cml-test >> check.input
 echo ,config ,load =scheme48/srfi/test-packages.scm >> check.input
 echo ,open portable-srfi-test >> check.input
 echo ,open test-suites >> check.input
-echo (define-test-suite all-tests (compiler-tests big-tests misc-tests tcp-sockets-tests udp-sockets-tests sort-tests r6rs-tests cml-tests portable-srfi-tests)) >> check.input
+echo (define-test-suite all-tests (compiler-tests big-tests misc-tests tcp-sockets-tests sort-tests r6rs-tests cml-tests portable-srfi-tests)) >> check.input
 echo (run-test-suite all-tests) >> check.input
 
 %runnable% -h 8000000 < check.input
