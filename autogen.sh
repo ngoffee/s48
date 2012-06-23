7c853f283450283c25b157f339b466a43f49b9e2
Mike Sperber <sperber@deinprogramm.de>
2012-06-23 15:58:40 +0200

    Make autogen.sh work with on FreeBSD, with FreeBSD make.
    Submitted by: Robert Ransom

--- autogen.sh
+++ autogen.sh
@@ -1,19 +1,24 @@
-#! /bin/sh 
+#!/bin/sh -e
 
 # Part of Scheme 48 1.9.  See file COPYING for notices and license.
 #
-# Authors: Martin Gasbichler, Marcus Crestani
+# Authors: Martin Gasbichler, Marcus Crestani, Robert Ransom
 #
 
+# Ensure that $MAKE is set.
+echo "${MAKE:=make}" >/dev/null
+
 echo "This script requires an installed scheme48 executable in the path."
-echo "The executable should be Scheme 48 1.6 or later." &&
-sleep 3 &&
-ACLOCAL="aclocal -I m4" autoreconf -v -i &&
-./configure &&
-rm -rf autom4te.cache &&
-rm -f scheme48.image build/initial.image-32 build/initial.image-64 c/scheme48.h &&
-make build/filenames.make &&
-make i-know-what-i-am-doing &&
-make c/scheme48.h &&
-make build/initial.image-32 build/initial.image-64 &&
-make distclean
+echo "The executable should be Scheme 48 1.6 or later."
+sleep 3
+ACLOCAL="aclocal -I m4" autoreconf -v -i
+./configure
+rm -rf autom4te.cache
+rm -f scheme48.image build/initial.image-32 build/initial.image-64 c/scheme48.h
+>build/filenames.make
+touch -t 197001010101 build/filenames.make
+${MAKE} ./build/filenames.make
+${MAKE} i-know-what-i-am-doing
+${MAKE} c/scheme48.h
+${MAKE} ./build/initial.image-32 ./build/initial.image-64
+${MAKE} distclean
